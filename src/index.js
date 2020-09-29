f4a41e48f8ffc21f6ac63818624858a1356aca2a
maxxxLmixxx-rs <max.280599@gmail.com>
2020-09-29 11:24:56 +0300

    feat: solve v-1.0

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  if (!Array.isArray(array) || !array.length) return 0;
+  return Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  if (!Array.isArray(array) || !array.length) return 0;
+  return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  if (!Array.isArray(array) || !array.length) return 0;
+  return array.reduce((sum, el) => sum + el, 0) / array.length;
 }
